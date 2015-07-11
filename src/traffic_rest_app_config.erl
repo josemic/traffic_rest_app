e1110bf41294414b767c28fb8f0fccf08714bccd
Michael Josenhans <michael.josenhans@web.de>
2015-07-11 17:28:44 +0200

    Added resource files traffic_rest_app_fetch_resource and traffic_rest_app_update_resource

--- src/traffic_rest_app_config.erl
+++ src/traffic_rest_app_config.erl
@@ -1,23 +1,25 @@
 -module(traffic_rest_app_config).
 
 -export([
-    dispatch/0,
-    web_config/0
+  dispatch/0,
+  web_config/0
 ]).
 
 -spec dispatch() -> [webmachine_dispatcher:route()].
 dispatch() ->
-    lists:flatten([
-        {[], traffic_rest_app_resource, []}
-    ]).
+  lists:flatten([
+    {["fetch", '*'], traffic_rest_app_fetch_resource, [{trace_dir, "/tmp/traces"}]},
+    {["update", '*'], traffic_rest_app_update_resource, []}
+  ]).
 
 web_config() ->
-    {ok, App} = application:get_application(?MODULE),
-    {ok, Ip} = application:get_env(App, web_ip),
-    {ok, Port} = application:get_env(App, web_port),
-    [
-        {ip, Ip},
-        {port, Port},
-        {log_dir, "priv/log"},
-        {dispatch, dispatch()}
-    ].
+  {ok, App} = application:get_application(?MODULE),
+  {ok, Ip} = application:get_env(App, web_ip),
+  {ok, Port} = application:get_env(App, web_port),
+  [
+    {ip, Ip},
+    {port, Port},
+    {log_dir, "priv/log"},
+    {dispatch, dispatch()}
+  ].
+  
